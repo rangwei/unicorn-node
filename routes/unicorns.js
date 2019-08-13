18ad78750c43942fc33b1d047159046412075fea
rangwei <rangwei@gmail.com>
2019-08-13 10:30:34 +0800

    add api

--- routes/unicorns.js
+++ routes/unicorns.js
@@ -1,14 +1,32 @@
 var express = require('express');
 var router = express.Router();
 
-
-
 /* GET unicorn list. */
 router.get('/', function(req, res) {
   var db = req.db;
   var collection = db.get('Unicorns');
   collection.find({},{},function(e,docs){
-    res.json(docs);
+    var doc = docs[0];
+    var unicorns = doc.entities.map(item => {
+      var unicorn = item;
+      delete unicorn["funding_rounds"];
+      return unicorn;
+    });
+    res.json(unicorns);
+  });
+});
+
+/* GET unicorn list. */
+router.get('/:name', function(req, res) {
+  var db = req.db;
+  var collection = db.get('Unicorns');
+  collection.find({},{},function(e,docs){
+
+    var doc = docs[0];
+
+    var result = doc.entities.find( element => element.name == req.params.name );
+
+    res.json(result);
   });
 });
 
